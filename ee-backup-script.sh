7b507388274b3d9fb371f62b8e77672b1509a251
Crypto | Seb <root@cryptoseb.pw>
2020-11-24 23:36:52 -0600

    Update ee-backup-script.sh
    I got some help from an individual on a coding Discord server. We went through and reformatted the code and he explained how it was wrong, and why I really shouldn't clutter it up with colours. I was having a lot of trouble with it not grabbing the server names properly and easy engine would fail when trying to backup the db file. Decided instead to print each site itself and just have lines that can be uncommented to allow for more than one site backup on the same box. Still struggling with the script deleting the .sql file from BOTH the original location (which is what we want) and from the backup location (not what we want).

--- ee-backup-script.sh
+++ ee-backup-script.sh
@@ -1,39 +1,63 @@
 #!/bin/bash
-PATH="/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin"
-EE_PATH="/opt/easyengine/sites"
-BACKUP_DIR_C="/root/ee-backups-ss/"
-BACKUP_PATH="/root"
-DATE=`date +%Y-%m-%d`
-CHOWN="root:root"
 
-RED='\033[0;31m'
-YELLOW='\033[1;33m'
-LIGHT_GREEN='\033[1;32m'
-LIGHT_GRAY='\033[0;37m'
-NC='\033[0m'
+# I had help with best-practice and formatting from sampersand[hashtag]8419.
+# If you are a future employer, dont go looking at the version history, haha.
 
-for SITES in $(ls $EE_PATH)
-do
-echo " "
-echo " "
-echo "${YELLOW}================= ${NC}[ ${LIGHT_GREEN}Starting ${NC}EasyEngine Backup Script by @CryptoSeb ] ${YELLOW}=================${NC}"
-echo " "
-echo "Making backup folder in ${LIGHT_GRAY}${BACKUP_PATH}${NC}"
-mkdir -p ${BACKUP_DIR_C}${DATE}
-echo " "
-echo "Backing up WordPress database to ${LIGHT_GRAY}webroot${NC}"
-ee shell ${SITES} --command="wp db export ${SITES}.sql"
-echo " "
-echo "Copying ${LIGHT_GRAY}${SITES} ${NC}to ${LIGHT_GRAY}${BACKUP_DIR_C}${NC}"
-cp -r ${EE_PATH} ${BACKUP_DIR_C}${DATE}
-echo " "
-echo "Removing ${LIGHT_GRAY}${SITES}.sql ${NC}backup file from ${LIGHT_GRAY}webroot${NC}"
-rm ${EE_PATH}/${SITES}/app/htdocs/${SITES}.sql
-echo " "
-echo "Changing ownership of ${LIGHT_GRAY}${BACKUP_DIR_C} ${NC}to ${LIGHT_GRAY}${CHOWN}${NC}"
-chown -R root:root ${BACKUP_DIR_C}
-echo " "
-echo "${YELLOW}================= ${NC}[ ${RED}Finished ${NC}EasyEngine Backup Script by @CryptoSeb ] ${YELLOW}=================${NC}"
-echo " "
-echo " "
-done
+# Utility to crash with a message if something happens.
+abort () { echo "$1" 1>&2; exit 1; }
+
+# the `: ${varname:=default}` syntax allows the callers of this script to
+# define the variables. if they aren't defined, the default values are used.
+: "${ee_path:=/opt/easyengine/sites}"
+: "${backup_dir_c:=/root/ee-backups/}"
+: "${backup_path:=/root}"
+: "${date:="$(date +%Y-%m-%d)"}"
+: "${chown:=root:root}"
+
+: "${site1:=saskseats.ca}"
+: "${site2:=hurricanekids.ca}"
+: "${site3:=example.com}"
+
+    backup_dst="$backup_dir_c$date"
+
+    # Print header.
+    echo '================= [ Starting EasyEngine Backup Script by @CryptoSeb ] ================='
+
+    # Create a backup folder.
+    echo 
+    echo "Making backup folder in: $backup_dst"
+    mkdir -p "$backup_dst" || abort "unable to make backup dir '$backup_dst'."
+
+    # Backup the FIRST WordPress database.
+    echo 
+    echo "Backing up WordPress database(s) to: webroot"
+    ee shell "$site1" --command="wp db export $site1.sql" || abort 'EasyEngine db backup failed, see above'
+
+    # Backup the SECOND WordPress database.
+    # ee shell "$site2" --command="wp db export $site2.sql" || abort 'EasyEngine db backup failed, see above'
+
+    # Copy all sites to '$backup_dst'
+    echo
+    echo "Copying sites to: $backup_dst"
+    cp -r "$ee_path" "$backup_dst" || abort "unable to copy all files from '$ee_path' to '$backup_dst'."
+
+    # Remove sql backup file for FIRST SITE from webroot.
+    echo
+    echo "Removing '$site1.sql' backup file from: webroot"
+    rm_path="$ee_path/$site1/app/htdocs/$site1.sql"
+    rm "$rm_path" || abort "unable to remove sql path: $rm_path."
+
+    # Remove sql backup file for SECOND SITE from webroot.
+    #echo
+    #echo "Removing '$site2.sql' backup file from: webroot"
+    # rm_path="$ee_path/$site1/app/htdocs/$site1.sql"
+    # rm "$rm_path" || abort "unable to remove sql path: $rm_path."
+
+    # change ownership of `backup_dir`.
+    echo
+    echo "Changing ownership of '$backup_dst' to: $chown"
+    chown -R "$chown" "$backup_dst" || abort "unable to change owner to '$chown' for '$backup_dst'."
+
+    # Print footer
+    echo
+    echo '================= [ Finished EasyEngine Backup Script by @CryptoSeb ] ================='
