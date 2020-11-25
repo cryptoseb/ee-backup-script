2e5a417caa89cfafc89012963fbde8757308b835
Crypto | Seb <root@cryptoseb.pw>
2020-11-24 20:52:20 -0600

    Update ee-backup-script.sh
    I learned about `for loops` and put it to use here to help solve my first idea. The script runs fine, and backs up the files, but when it goes to remove the .sql file from the original location that it dropped it (webroot), it also deletes it from the backup location. I don't yet know how to solve this.

--- ee-backup-script.sh
+++ ee-backup-script.sh
@@ -1,32 +1,34 @@
 #!/bin/bash
 PATH="/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin"
-EE_PATH="/opt/easyengine/sites/"
-BACKUP_DIR_C="/root/ee-backups/"
+EE_PATH="/opt/easyengine/sites"
+BACKUP_DIR_C="/root/ee-backups-ss/"
 BACKUP_PATH="/root"
 DATE=`date +%Y-%m-%d`
-SITE="example.com"
 CHOWN="root:root"
 
-PURPLE='\033[1;35m'
 RED='\033[0;31m'
 YELLOW='\033[1;33m'
-BLUE='\033[0;34m'
 LIGHT_GREEN='\033[1;32m'
-LIGHT_BLUE='\033[1;34m'
 LIGHT_GRAY='\033[0;37m'
 NC='\033[0m'
 
+for SITES in $(ls $EE_PATH)
+do
 echo " "
 echo " "
 echo "${YELLOW}================= ${NC}[ ${LIGHT_GREEN}Starting ${NC}EasyEngine Backup Script by @CryptoSeb ] ${YELLOW}=================${NC}"
 echo " "
 echo "Making backup folder in ${LIGHT_GRAY}${BACKUP_PATH}${NC}"
 mkdir -p ${BACKUP_DIR_C}${DATE}
+echo " "
 echo "Backing up WordPress database to ${LIGHT_GRAY}webroot${NC}"
-ee shell ${SITE} --command="wp db export ${SITE}.sql"
+ee shell ${SITES} --command="wp db export ${SITES}.sql"
+echo " "
+echo "Copying ${LIGHT_GRAY}${SITES} ${NC}to ${LIGHT_GRAY}${BACKUP_DIR_C}${NC}"
 cp -r ${EE_PATH} ${BACKUP_DIR_C}${DATE}
-echo "Removing ${LIGHT_GRAY}${SITE}.sql ${NC}backup file from ${LIGHT_GRAY}webroot${NC}"
-rm ${EE_PATH}${SITE}/app/htdocs/${SITE}.sql
+echo " "
+echo "Removing ${LIGHT_GRAY}${SITES}.sql ${NC}backup file from ${LIGHT_GRAY}webroot${NC}"
+rm ${EE_PATH}/${SITES}/app/htdocs/${SITES}.sql
 echo " "
 echo "Changing ownership of ${LIGHT_GRAY}${BACKUP_DIR_C} ${NC}to ${LIGHT_GRAY}${CHOWN}${NC}"
 chown -R root:root ${BACKUP_DIR_C}
@@ -34,3 +36,4 @@ echo " "
 echo "${YELLOW}================= ${NC}[ ${RED}Finished ${NC}EasyEngine Backup Script by @CryptoSeb ] ${YELLOW}=================${NC}"
 echo " "
 echo " "
+done
